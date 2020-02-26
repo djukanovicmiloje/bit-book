fa8391f9b5ff4addaab83f3ffe18a8f22890f9b7
djukanovicmiloje <djukanovicmiloje97@gmail.com>
2020-02-26 20:37:13 +0100

    setup store provider and hashrouter

---
+++ src/view/BitBook.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../store/store";
+import { HashRouter, Switch } from "react-router-dom";
+
+const App = () => (
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={SignUpPage} />
+      </Switch>
+    </HashRouter>
+  </Provider>
+);
+
+export default App;
